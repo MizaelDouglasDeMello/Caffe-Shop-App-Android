d899fe02f007a8af4f5dd52a6f455bd02e15b73c
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-11-07 14:29:04 -0300

    feat: refactor app

--- app/src/main/java/br/com/mizaeldouglas/coffe_shop_mobile/di/AppModule.kt
+++ app/src/main/java/br/com/mizaeldouglas/coffe_shop_mobile/di/AppModule.kt
@@ -2,9 +2,6 @@ package br.com.mizaeldouglas.coffe_shop_mobile.di
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
-import br.com.mizaeldouglas.coffe_shop_mobile.data.remote.CoffeeFetcher
-import br.com.mizaeldouglas.coffe_shop_mobile.data.remote.CoffeeInserter
-import br.com.mizaeldouglas.coffe_shop_mobile.data.remote.DatabaseHelper
 import br.com.mizaeldouglas.coffe_shop_mobile.data.remote.FirestoreManager
 import br.com.mizaeldouglas.coffe_shop_mobile.data.repository.CoffeeRepositoryImpl
 import br.com.mizaeldouglas.coffe_shop_mobile.domain.repository.ICoffeRepository
@@ -18,30 +15,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
-
-    @Provides
-    @Singleton
-    fun provideDatabaseHelper(@ApplicationContext context: Context): DatabaseHelper {
-        return DatabaseHelper(context)
-    }
-
-    @Provides
-    @Singleton
-    fun provideSQLiteDatabase(databaseHelper: DatabaseHelper): SQLiteDatabase {
-        return databaseHelper.writableDatabase
-    }
-
-    @Provides
-    @Singleton
-    fun provideCoffeeFetcher(database: SQLiteDatabase): CoffeeFetcher {
-        return CoffeeFetcher(database)
-    }
-
-    @Provides
-    @Singleton
-    fun provideCoffeeInserter(database: SQLiteDatabase): CoffeeInserter {
-        return CoffeeInserter(database)
-    }
 
     @Provides
     @Singleton
